aac3d0ed10315a57cd625a8836bf7bf0b857fd0e
Soumya <soumyakantid5@gmail.com>
2022-12-30 21:24:45 +0530

    .env added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// # PLEASE READ THE README FILE FIRST AND THIS IS NOT THE FINAL BRANCH, PLEASE DO TESTING ON FINAL BRANCH "project/blog"
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const route = require("./routes/route.js");
@@ -8,21 +8,17 @@ const multer = require('multer')
 const app = express();
 app.use(bodyParser.json());
 app.use(multer().any());
-app.use("/functionup", route);
 
-function start(){
-  mongoose.connect("mongodb+srv://soumya-db:afdbyZgt3CyQporD@cluster0.gvqtfzu.mongodb.net/Project2_OpenToIntern",
-    {
-      useNewUrlParser: true,
-    }
-  )
-  .then(() => console.log("Connected with Database ✅✅✅"))
+
+
+  mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true,})
+  .then(() => console.log("Connected with Database"))
   .catch((err) => console.log(err));
 
+  app.use("/functionup", route);
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Express app running on port " + (process.env.PORT || 3000));
+app.listen(process.env.PORT || 5000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 5000));
 });
-}
 
-start();
+
