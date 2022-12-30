a7cb8daa25a5c9107b2936da85c26bf0eb19b956
Soumya <soumyakantid5@gmail.com>
2022-12-30 22:14:45 +0530

    bug fix

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,10 +4,10 @@ const collegeController = require("../controllers/collegeControllers");
 const internControllers = require("../controllers/internControllers");
 
 //POST APIS TO CREATE COLLEGE AND INTERNS
-router.post("/functionup/colleges", collegeController.createCollege);
-router.post("/functionup/interns", internControllers.createInterns);
+router.post("/colleges", collegeController.createCollege);
+router.post("/interns", internControllers.createInterns);
 
 //GET API TO FETCH THE COLLEGE DETAILS
-router.get("/functionup/collegeDetails", collegeController.getCollege);
+router.get("/collegeDetails", collegeController.getCollege);
 
 module.exports = router;
